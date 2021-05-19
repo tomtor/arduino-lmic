3cf4f5c674a456f042c71b75561e22e3559ae830
Tom Vijlbrief <tvijlbrief@gmail.com>
2021-05-19 10:01:15 +0200

    Disable optional code

--- project_config/lmic_project_config.h
+++ project_config/lmic_project_config.h
@@ -8,3 +8,8 @@
 //#define CFG_in866 1
 #define CFG_sx1276_radio 1
 //#define LMIC_USE_INTERRUPTS
+
+#define DISABLE_PING
+// define this in lmic_project_config.h to disable all code related to beacon tracking.
+// Requires ping to be disabled too
+#define DISABLE_BEACONS
